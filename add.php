b09e8f2346f098993d8b659ceaeda9824eb97a57
Limuw <itsLimuw@gmail.com>
2021-12-07 13:00:22 +0300

    buttons

--- add.php
+++ add.php
@@ -14,8 +14,6 @@ $arr = [
     $_GET["customercompany"]]
 ];
 
-
-
 $cleardb_url = parse_url(getenv('CLEARDB_DATABASE_URL'));
 $cleardb_server = $cleardb_url['host'];
   $cleardb_username = $cleardb_url['user'];
@@ -29,4 +27,17 @@ $cleardb_server = $cleardb_url['host'];
     echo json_encode('died from cringe  ');
     die('Connection failed: ' . $conn -> connect_error);
   }
+
+  $sql = 'INSERT INTO road (id, name, password, rating, marked, pfp) VALUES ('.$_GET['id'].', \''.$_GET['name'].'\', \''.$_GET['password'].'\','.$_GET['rating'].', '.$_GET['marked'].', '.$_GET['pfp'].')';
+  $result = mysqli_query($conn, $sql);
+  $users = mysqli_fetch_all($result);
+
+
+
+
+
+
+
+
+
 ?>
