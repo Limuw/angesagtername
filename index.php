8ece420801c4f87a68d38a6cad7f581463c947a2
Limuw <itsLimuw@gmail.com>
2021-12-08 17:01:52 +0300

    kekl

--- index.php
+++ index.php
@@ -8,58 +8,56 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+<body>
 <div class="container mt-4">
-                <h1>Добавление дороги</h1>
-        <form action="add.php" method="GET">
-            <input type="text" class="form-control" name="roadname" id="roadname" placeholder="Введите название дороги"  autocomplete = "off" required autofocus><br>
-            <input type="number" class="form-control" name="roadlength" id="roadlength" placeholder="Введите длину дороги"  autocomplete = "off" required><br>
-            <input type="number" class="form-control" name="roadxstart" id="roadxstart" placeholder="Введите X координату начальной точки дороги"  autocomplete = "off" required><br>
-            <input type="number" class="form-control" name="roadystart" id="roadystart" placeholder="Введите Y координату начальной точки дороги"  autocomplete = "off" required><br>
-            <input type="number" class="form-control" name="roadxend" id="roadxend" placeholder="Введите X координату конечной точки дороги"  autocomplete = "off" required><br>
-            <input type="number" class="form-control" name="roadyend" id="roadyend" placeholder="Введите Y координату конечной точки дороги"  autocomplete = "off" required><br>
-            <!-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
-            <input type="text" class="form-control" name="customername" id="customername" placeholder="Введите имя заказчика"  autocomplete = "off" required><br>
-            <input type="text" class="form-control" name="customersname" id="customersname" placeholder="Введите фамилию заказчика"  autocomplete = "off" required><br>
-            <input type="email" class="form-control" name="customeremail" id="customeremail" placeholder="customeremail@gmail.com"  autocomplete = "off" required><br>
-            <input type="tel" class="form-control" name="customerphone" id="customerphone" placeholder="Введите номер заказчика"  autocomplete = "off" required><br>
-            <input type="text" class="form-control" name="customercompany" id="customercompany" placeholder="Введите компанию заказчика"  autocomplete = "off" required><br>
-            <button type="submit" class="btn btn-success">Добавить</button>
-            <button type="reset" class="btn btn-success">Сброс</button>
-            <!-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
-        </form>
+    <h1>Добавление дороги</h1>
+    <form action="add.php" method="GET">
+        <input type="text" class="form-control" name="roadname" id="roadname" placeholder="Введите название дороги"  autocomplete = "off" required autofocus><br>
+        <input type="number" class="form-control" name="roadlength" id="roadlength" placeholder="Введите длину дороги"  autocomplete = "off" required><br>
+        <input type="number" class="form-control" name="roadxstart" id="roadxstart" placeholder="Введите X координату начальной точки дороги"  autocomplete = "off" required><br>
+        <input type="number" class="form-control" name="roadystart" id="roadystart" placeholder="Введите Y координату начальной точки дороги"  autocomplete = "off" required><br>
+        <input type="number" class="form-control" name="roadxend" id="roadxend" placeholder="Введите X координату конечной точки дороги"  autocomplete = "off" required><br>
+        <input type="number" class="form-control" name="roadyend" id="roadyend" placeholder="Введите Y координату конечной точки дороги"  autocomplete = "off" required><br>
+        <!-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+        <input type="text" class="form-control" name="customername" id="customername" placeholder="Введите имя заказчика"  autocomplete = "off" required><br>
+        <input type="text" class="form-control" name="customersname" id="customersname" placeholder="Введите фамилию заказчика"  autocomplete = "off" required><br>
+        <input type="email" class="form-control" name="customeremail" id="customeremail" placeholder="customeremail@gmail.com"  autocomplete = "off" required><br>
+        <input type="tel" class="form-control" name="customerphone" id="customerphone" placeholder="Введите номер заказчика"  autocomplete = "off" required><br>
+        <input type="text" class="form-control" name="customercompany" id="customercompany" placeholder="Введите компанию заказчика"  autocomplete = "off" required><br>
+        <button type="submit" class="btn btn-success">Добавить</button>
+        <button type="reset" class="btn btn-success">Сброс</button>
+        <!-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+    </form>
 </div>
-                <div>
-                
-                    <?php
-                        $cleardb_url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-                        $cleardb_server = $cleardb_url['host'];
-                        $cleardb_username = $cleardb_url['user'];
-                        $cleardb_password = $cleardb_url['pass'];
-                        $cleardb_db = substr($cleardb_url['path'],1);
-                        $active_group = 'default';
-                        $query_builder = TRUE;
-                        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-                        if ($conn -> connect_error) {
-                        echo json_encode('died from cringe  ');
-                        die('Connection failed: ' . $conn -> connect_error);
-                        }
+<div>
 
-                        $sqlSelectRoad = 'SELECT * FROM road';
-                        $resultRoad = mysqli_query($conn, $sqlSelectRoad);
-                        $road = mysqli_fetch_all($resultRoad);
-                        
-                        $str = "<select>"; 
-                        for($i=0; $i<count($road); $i++){
-                            $str.="<option value=".$i.">".$road[$i][1]."</option>";
-                        
-                        }
-                        $str.="</select>";
-                        echo $str;
+    <?php
+        $cleardb_url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+        $cleardb_server = $cleardb_url['host'];
+        $cleardb_username = $cleardb_url['user'];
+        $cleardb_password = $cleardb_url['pass'];
+        $cleardb_db = substr($cleardb_url['path'],1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        if ($conn -> connect_error) {
+        echo json_encode('died from cringe  ');
+        die('Connection failed: ' . $conn -> connect_error);
+        }
 
-                    ?>
-                    
-                
-            </div>
-    
+        $sqlSelectRoad = 'SELECT * FROM road';
+        $resultRoad = mysqli_query($conn, $sqlSelectRoad);
+        $road = mysqli_fetch_all($resultRoad);
+        
+        $str = "<select>"; 
+        for($i=0; $i<count($road); $i++){
+            $str.="<option value=".$i.">".$road[$i][1]."</option>";
+        
+        }
+        $str.="</select>";
+        echo $str;
+    ?>
+</div>
+    </body>    
 </html>
 
